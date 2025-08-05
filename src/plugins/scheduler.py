b0bcf57eaa74d6f7f40e5dedbee96fd6e7ebaee4
mobu <mobu54511@gmail.com>
2025-08-05 11:41:47 +0800

    revise num  on command scheduler3

--- src/plugins/scheduler.py
+++ src/plugins/scheduler.py
@@ -93,7 +93,6 @@ async def send_daily_digest():
                     line.append(f"({i+1}) 这篇《{item['title']}》看起来很有趣！")
                     line.append(f"茉子点评：{item['description']}") 
                     line.append(f"传送门→") 
-                    line.append(f"") 
                     line.append(item['url']) 
             else:
                 line.append("    欸~？这个板块今天居然是空空如也啊，茉子也没找到好玩的…… ( ´･ω･)")
@@ -135,7 +134,6 @@ async def _(matcher:Matcher):
                     line.append(f"({i+1}) 这篇《{item['title']}》看起来很有趣！")
                     line.append(f"茉子点评：{item['description']}") 
                     line.append(f"传送门→") 
-                    line.append(f"") 
                     line.append(item['url']) 
             else:
                 line.append("    欸~？这个板块今天居然是空空如也啊，茉子也没找到好玩的…… ( ´･ω･)")
